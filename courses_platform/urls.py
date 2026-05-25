d58b4350c0c304c64d79738a43e72146a82e8f53
mousa469 <mohamed.mouza469@gmail.com>
2026-05-25 16:25:22 +0300

    create all courses  and course details pages

--- courses_platform/urls.py
+++ courses_platform/urls.py
@@ -20,5 +20,6 @@ from accounts.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/' , include("accounts.urls")),
+    path("courses/" , include("courses.urls")),
     path('' , home)
 ]

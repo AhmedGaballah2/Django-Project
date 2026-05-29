348f3613a3958cd2b57ed7e31f6e603685103d8c
mousa469 <mohamed.mouza469@gmail.com>
2026-05-29 13:48:50 +0300

    setup django rest

--- courses_platform/settings.py
+++ courses_platform/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
     'courses',
     'accounts',
     'django.contrib.admin',
@@ -125,3 +127,15 @@ STATICFILES_DIRS = [
 LOGIN_URL="/accounts/login"
 LOGIN_REDIRECT_URL= "/"
 LOGOUT_REDIRECT_URL = "/accounts/login"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+         'rest_framework.authentication.TokenAuthentication',
+         'rest_framework.authentication.SessionAuthentication'
+    ],
+    "DEFAULT_PERMISSION_CLASSES":[
+        'rest_framework.permissions.IsAuthenticated'
+    ]
+}
+    

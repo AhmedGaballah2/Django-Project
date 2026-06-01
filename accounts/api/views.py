4b04509befad1d8949c9109cd568e078397b7885
mousa469 <mohamed.mouza469@gmail.com>
2026-06-01 19:42:39 +0300

    add role to registration

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import  AllowAny
+from ..models import Profile
 
 
 
@@ -17,13 +18,20 @@ class RegisterUser(APIView):
      def post(self ,request):
           username = request.data.get("username")
           password = request.data.get("password")
+          role = request.data.get("role")
+
+          if not username or not password or not role:
+               return Response({"error":"username,password and role are required"} , status.HTTP_400_BAD_REQUEST)
           
 
-          if not username or not password:
-               return Response({"error":"username and password are required"} , status.HTTP_400_BAD_REQUEST)
-          
+          if role not in ["student" , "instructor"]:
+               return Response({"error":"Invalid role"} , status.HTTP_400_BAD_REQUEST)
+
+
+
           if User.objects.filter(username=username).exists():
                 return Response({"error":"username already exist"} , status.HTTP_400_BAD_REQUEST)
+          
           
 
 
@@ -32,6 +40,7 @@ class RegisterUser(APIView):
 
 
           user = User.objects.create(username=username,password=password)
+          profile = Profile.objects.create(owner=user,role=role)
 
           token = Token.objects.create(user=user)
           return Response({"token":token.key} , status.HTTP_201_CREATED)

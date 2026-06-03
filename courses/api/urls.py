71f39ff32437dffff25bfe1a8e2a7b1b3de2b924
mousa469 <mohamed.mouza469@gmail.com>
2026-06-03 21:48:33 +0300

    fix redirection

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -9,9 +9,9 @@ urlpatterns=[
     # path("courses" , views.courses_list , name="courses"),
     path("courses" , views.CoursesAPIView.as_view()),
     # path("courses/<int:id>" , views.single_course , name="single_course"),
-    path("courses/<int:id>" , views.CoursesAPIView.as_view() , name="single_course"),
+    path("courses/<int:id>" , views.CoursesAPIView.as_view() ),
 
-    path("my-enrollments" , views.StudentEnrollmentsAPIView.as_view() , name = "student-enrollment"),
+    path("my-enrollments" , views.StudentEnrollmentsAPIView.as_view() ),
 
     # path("my-enrollments" , views.student_enrollment , name = "student-enrollment"),
     
